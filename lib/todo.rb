94ea1690c50ec3bb309e622808e2dc73a2a8bfc2
sarahc-dev <sarahclements@hotmail.co.uk>
2023-04-13 14:55:48 +0100

    add mark as complete

--- lib/todo.rb
+++ lib/todo.rb
@@ -11,4 +11,9 @@ class Todo
   def show_tasks
     @list.each { |x| puts "* #{x}" }
   end
+
+  def mark_as_complete(task)
+    @list.delete(task)
+    "#{task} completed"
+  end
 end
--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -16,4 +16,23 @@ describe Todo do
       expect { todo_list.show_tasks }.to output("* Do laundry\n").to_stdout
     end
   end
+
+  context "mark as complete and remove" do
+    it "removes task from list" do
+      todo_list = Todo.new
+      todo_list.add_task("Do laundry")
+      todo_list.add_task("Eat")
+      expect(todo_list.mark_as_complete("Eat")).to eq "Eat completed"
+    end
+  end
+
+  context "show list after removing task" do
+    it "shows the list" do
+      todo_list = Todo.new
+      todo_list.add_task("Do laundry")
+      todo_list.add_task("Eat")
+      todo_list.mark_as_complete("Eat")
+      expect { todo_list.show_tasks }.to output("* Do laundry\n").to_stdout
+    end
+  end
 end
